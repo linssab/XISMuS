51bb662867683e75ebf09fcfbf5911b4b6ec8bb8
linssab <sergiolins_@outlook.com>
2019-03-08 00:26:47 +0100

    added single element mapping

--- Mapping.py
+++ Mapping.py
@@ -8,25 +8,27 @@
 import sys
 import os
 import numpy as np
+from multiprocessing import Process
 import SpecMath
 import SpecRead
 from PyMca5.PyMcaMath import SpecArithmetic as Arithmetic
 import matplotlib.pyplot as plt
 import time
+import math
 
 timer=time.time()
 #_for now the first spectra is given manually
 start = SpecRead.input
 
 imagsize = SpecRead.getdimension()
-x = imagsize[0]
-y = imagsize[1]
-dimension = x*y
+imagex = imagsize[0]
+imagey = imagsize[1]
+dimension = imagex*imagey
 
 def updateposition(a,b):
     currentx=a
     currenty=b
-    if currenty == y-1:
+    if currenty == imagey-1:
 #        print("We reached the end of the line, y= %d" % currenty)
         currenty=0
 #        print("Now we reset the column, y= %d" % currenty)
@@ -35,20 +37,14 @@ def updateposition(a,b):
     else:
         currenty+=1
 #        print("We are still in the middle of the line, y+1 = %d" % currenty)
-    if currentx == x-1 and currenty == y-1:
+    if currentx == imagex-1 and currenty == imagey-1:
         print("END OF IMAGE")
     actual=([currentx,currenty])
     return actual
 
-def getpeaks(file):
-    data = SpecRead.getdata(file)
-    data = np.asarray(data, dtype='int')
-    energies = SpecRead.calibrate(file)
-    return Arithmetic.search_peak(energies,data)
-
-def createmap():
+def plotdensitymap():
     currentspectra = start
-    density_map = np.zeros([x,y])
+    density_map = np.zeros([imagex,imagey])
     scan=([0,0])
     currentx=scan[0]
     currenty=scan[1]
@@ -67,4 +63,29 @@ def createmap():
     plt.imshow(density_map,cmap='gray')
     plt.show()
 
-createmap()
+def plotpeakmap(element):
+    currentspectra = start
+    density_map = np.zeros([imagex,imagey])
+    scan=([0,0])
+    currentx=scan[0]
+    currenty=scan[1]
+    energyaxis = SpecRead.calibrate(currentspectra,'data')
+
+    for ITERATION in range(dimension):
+        spec = currentspectra
+    #    print("Current X= %d\nCurrent Y= %d" % (currentx,currenty))
+    #    print("Current spectra is: %s" % spec)
+        sum = SpecMath.getpeakarea(spec,element,energyaxis)
+        density_map[currentx][currenty]=sum          #_update matrix
+        scan=updateposition(scan[0],scan[1])
+        currentx=scan[0]
+        currenty=scan[1]
+        currentspectra = SpecRead.updatespectra(spec,dimension)
+        if isinstance(timer,int): print("Partial! %s seconds!" % (time.time() - timer))    
+    print("Execution took %s seconds" % (time.time() - timer))    
+    plt.imshow(density_map,cmap='gray')
+    plt.show()
+
+if __name__=="__main__":
+    elementen = int(float(sys.argv[1])*1000)
+    plotpeakmap(elementen)
