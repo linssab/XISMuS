9f3949b9890739759cbcd3a9afbe4436f23ca64e
linssab <sergiolins_@outlook.com>
2019-03-12 17:35:08 +0100

    merged ThicknessMatrix together added -getratio x as sys argv

--- Mapping.py
+++ Mapping.py
@@ -1,14 +1,12 @@
 #################################################################
 #                                                               #
-#          DENSITY MAP GENERATOR                                #
-#                        version: a1.3                          #
+#          XRF MAP GENERATOR                                    #
+#                        version: a1.4.0                        #
 # @author: Sergio Lins               sergio.lins@roma3.infn.it  #
 #################################################################
 
 import sys
-import os
 import numpy as np
-from multiprocessing import Process
 import SpecMath
 import SpecRead
 import EnergyLib
@@ -18,7 +16,13 @@ import matplotlib.pyplot as plt
 import time
 import cv2
 import math
+import logging
 
+logging.basicConfig(format = '%(asctime)s\t%(levelname)s\t%(message)s',
+filename = 'process.log',level = logging.INFO)
+f=open('process.log','w')
+f.truncate(0)
+logging.info('*'* 10 + ' MAPPING.PY LOG START ' + '*'* 10)
 timer=time.time()
 # FOR NOW THE STARTING SPECTRA MUST BE GIVEN MANUALLY IN SpecRead.py FILE
 start = SpecRead.input
@@ -51,7 +55,7 @@ def plotdensitymap():
     scan=([0,0])
     currentx=scan[0]
     currenty=scan[1]
-
+    logging.info("Started acquisition of density map")
     for ITERATION in range(dimension):
         spec = currentspectra
 #        print("Current X= %d\nCurrent Y= %d" % (currentx,currenty))
@@ -62,6 +66,7 @@ def plotdensitymap():
         currentx=scan[0]
         currenty=scan[1]
         currentspectra = SpecRead.updatespectra(spec,dimension)
+    logging.info("Finished fetching density map!")
     print("Execution took %s seconds" % (time.time() - timer))
     density_map = density_map.astype(np.float32)/density_map.max()
     density_map = 255*density_map
@@ -70,28 +75,33 @@ def plotdensitymap():
     plt.show()
     return image
 
-def plotpeakmap(*args):
+def plotpeakmap(*args,ratio=None,plot=None):
     partialtimer = time.time()
     LocalElementList = args
     colorcode=['red','green','blue']
     eleenergy = EnergyLib.Energies
+    logging.info("Started energy axis calibration")
+    energyaxis = SpecRead.calibrate(start,'data')
+    logging.info("Finished energy axis calibration")
     stackimage = colorize(np.zeros([imagex,imagey]))
     for input in range(len(LocalElementList)):
         Element = LocalElementList[input]
-        
         # STARTS IMAGE ACQUISITION FOR ELEMENT 'Element'
         
         if Element in Elements.ElementList:
+            logging.info("Started acquisition of {0} map".format(Element))
             print("Fetching map image for %s..." % Element)
             pos = Elements.ElementList.index(Element) 
             element = eleenergy[pos]*1000
             currentspectra = start
-            energyaxis = SpecRead.calibrate(currentspectra,'data')
             elmap = np.zeros([imagex,imagey])
             scan=([0,0])
             currentx=scan[0]
             currenty=scan[1]
-        
+            if ratio == True: 
+                ratiofile = open('ratio.txt','w+')
+                logging.warning("Ratio map will be generated!")
+            logging.info("Starting iteration over spectra...")
             for ITERATION in range(dimension):
                 spec = currentspectra
 #               print("Current X= %d\nCurrent Y= %d" % (currentx,currenty))
@@ -99,19 +109,35 @@ def plotpeakmap(*args):
                 specdata = SpecRead.getdata(spec)
                 sum = SpecMath.getpeakarea(specdata,element,energyaxis)
                 elmap[currentx][currenty] = sum
+                
+                if ratio == True:
+                    ka = sum
+                    kbindex = Elements.ElementList.index(Element) 
+                    kbenergy = EnergyLib.kbEnergies[kbindex]*1000
+                    kb = SpecMath.getpeakarea(specdata,kbenergy,energyaxis)
+                    row = scan[0]
+                    column = scan[1]
+                    ratiofile.write("%d\t%d\t%d\t%d\n" % (row, column,ka, kb))
+
                 scan = updateposition(scan[0],scan[1])
                 currentx = scan[0]
                 currenty = scan[1]
                 currentspectra = SpecRead.updatespectra(spec,dimension)
+            logging.info("Finished iteration process for element {}".format(Element))
+            if ratio == True: ratiofile.close()
+            logging.info("Started normalizing and coloring step")
             image = elmap/normalize(energyaxis)*255
             tintimage = colorize(image,colorcode[input+1])
             stackimage = cv2.addWeighted(tintimage, 1, stackimage, 1, 0)
+            logging.info("Finished normalizing and coloring step")
             print("Execution took %s seconds" % (time.time() - partialtimer))
             partialtimer = time.time()
-    plt.imshow(stackimage)
-    cax = plt.axes([0,255,1,10])
-    plt.colorbar(cax=cax)
-    plt.show()
+    logging.info("Finished map acquisition!")
+    if plot == True: 
+        plt.imshow(stackimage)
+        cax = plt.axes([0,255,1,10])
+        plt.colorbar(cax=cax)
+        plt.show()
     return stackimage
 
 def normalize(energyaxis):
@@ -170,11 +196,43 @@ def colorize(elementmap,color=None):
 if __name__=="__main__":
     flag1 = sys.argv[1]
     if flag1 == '-help':
-        print("USAGE: '-findelement x'; plots a 2D map of the element 'x'\n\
+        print("USAGE: '-findelement x y'; plots a 2D map of elements 'x' and/or 'y'\n\
        '-plotmap'; plots a density map\n\
-       '-plotstack'; plots the sum spectra of all sample. Optional: you can add '-semilog' to plot it in semilog mode.")
+       '-plotstack'; plots the sum spectra of all sample. Optional: you can add '-semilog' to plot it in semilog mode.\n\
+       '-getratios x'; creates the ka/kb or la/lb ratio image for element 'x'. K or L are chosen accordingly.")
     if flag1 == '-findelement':    
-        if len(sys.argv) > 4: raise Exception("More than 2 elements were selected! Try again with 2 or less inputs!")
+        if len(sys.argv) > 4: 
+            raise Exception("More than 2 elements were selected! Try again with 2 or less inputs!")
+            logging.exception("More than 2 elements were selected!")
+        if len(sys.argv) > 2:
+            element1 = None
+            element2 = None
+            if sys.argv[2] in Elements.ElementList:
+                element1 = sys.argv[2]
+            else: 
+                raise Exception("%s not an element!" % sys.argv[2])
+                logging.exception("{0} is not a chemical element!".format(sys.argv[2]))
+            if len(sys.argv) > 3:
+                if sys.argv[3] in Elements.ElementList:
+                    element2 = sys.argv[3]
+                else: 
+                    raise Exception("%s not an element!" % sys.argv[3])
+                    logging.exception("{0} is not a chemical element!".format(sys.argv[3]))
+            plotpeakmap(element1,element2,plot=True)
+        else: 
+            raise Exception("No element input!")
+            logging.exception("No element input!")
+    if flag1 == '-plotmap':
+        print("Fetching density map...")
+        plotdensitymap()
+    if flag1 == '-plotstack':
+        if len(sys.argv) >= 3:
+            flag2 = sys.argv[2]
+        else:
+            flag2 = None
+        SpecRead.getstackplot(start,flag2)
+    if flag1 == '-getratios':
+        if len(sys.argv) > 3: raise Exception("More than one element selected!\nFor -getratios, please input only one element.")
         if len(sys.argv) > 2:
             element1 = None
             element2 = None
@@ -185,14 +243,13 @@ if __name__=="__main__":
                 if sys.argv[3] in Elements.ElementList:
                     element2 = sys.argv[3]
                 else: raise Exception("%s not an element!" % sys.argv[3])
-            plotpeakmap(element1,element2)
+            plotpeakmap(element1,element2,ratio=True)
+            ratiofile = 'ratio.txt'
+            ratiomatrix = SpecRead.RatioMatrixReadFile(ratiofile)
+            ratiomatrix = SpecRead.RatioMatrixTransform(ratiomatrix)
+            #ratioimage = colorize(ratiomatrix,'gray')
+            #cv2.addWeighted(ratioimage, 1, ratioimage, 0, 0)
+            plt.imshow(ratiomatrix,cmap='gray')
+            plt.show()
         else: raise Exception("No element input!")
-    if flag1 == '-plotmap':
-        print("Fetching density map...")
-        plotdensitymap()
-    if flag1 == '-plotstack':
-        if len(sys.argv) >= 3:
-            flag2 = sys.argv[2]
-        else:
-            flag2 = None
-        SpecRead.getstackplot(start,flag2)
+
