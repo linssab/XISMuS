0659b19d90a4f78f941fb2bd72edba687e9592e7
linssab <sergiolins_@outlook.com>
2019-04-15 17:48:54 +0200

    equal 3d axes function from stackoverflow added

--- ImgMath.py
+++ ImgMath.py
@@ -68,6 +68,39 @@ def getheightmap(depth_matrix,thickratio,compound,KaKb='Pb'):
             else: heightmap[i][j] = 10000 * d
     return heightmap
 
+#####################################################
+#   set_axes_equal(ax) FUNCTION OBTAINED FROM:      #
+#   https://stackoverflow.com/questions/13685386    #
+#####################################################
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
 def plot3D(depth_matrix):
     fig = plt.figure()
     ax = fig.gca(projection = '3d')
@@ -84,10 +117,10 @@ def plot3D(depth_matrix):
     depth_matrix = depth_matrix.transpose()
     MAP = ax.plot_surface(X,Y,depth_matrix,\
             cmap='BuGn',linewidth=0,antialiased=True)
-    ax.set_zlim(-1*depth_matrix.max(),depth_matrix.max()*2)
-#    ax.set_zlim(-10,10)
-    ax.set_ylim(-1,imagesize[1]*1.10)
-    ax.set_xlim(-1,imagesize[0]*1.10)
+#    ax.set_zlim(-1*depth_matrix.max(),depth_matrix.max()*2)
+#    ax.set_ylim(-1,imagesize[1]*1.10)
+#    ax.set_xlim(-1,imagesize[0]*1.10)
+    set_axes_equal(ax)
     plt.show()
     return 0
 
