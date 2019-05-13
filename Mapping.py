fixes to calculation modes

Mapping.py
@@ -32,7 +32,7 @@ dimension = imagex*imagey
 configdict = SpecRead.getconfig()
 
 def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
-        normalize=configdict.get('enhance'),svg=configdict.get('bgstrip'),\
+        normalize=configdict.get('enhance'),bgstrip=configdict.get('bgstrip'),\
         peakmethod=configdict.get('peakmethod')):
 
     ################-getpeakmap-#####################
@@ -51,7 +51,8 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
     current_peak_factor = 0
     max_peak_factor = 0
     ymax_spec = None
-
+    usedif2 = True
+    
     #########################################
     # ERROR VARIABLES CUMSUM AND CUMSUM_RAW #
     #########################################
@@ -82,7 +83,7 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
                     .format(Element) + 10*"-" + '\n')
             ratiofile.write("row\tcolumn\tline1\tline2\n")
             
-            if svg != 'None': logging.warning("Background stripping is ON! - slow -")
+            if bgstrip != 'None': logging.warning("Background stripping is ON! - slow -")
             
             logging.warning("Ratio map will be generated!")
             kbindex = Elements.ElementList.index(Element)
@@ -96,8 +97,8 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
 
         if peakmethod == 'simple_roi':
             stacksum = SpecMath.stacksum(currentspectra,dimension)
-            ka_idx = SpecMath.setROI(kaenergy,energyaxis,stacksum,svg=configdict.get('bgstrip'))
-            kb_idx = SpecMath.setROI(kbenergy,energyaxis,stacksum,svg=configdict.get('bgstrip'))
+            ka_idx = SpecMath.setROI(kaenergy,energyaxis,stacksum,bgstrip)
+            kb_idx = SpecMath.setROI(kbenergy,energyaxis,stacksum,bgstrip)
             print(ka_idx,kb_idx)
             ka_peakdata = stacksum[ka_idx[0]:ka_idx[1]]
             kb_peakdata = stacksum[kb_idx[0]:kb_idx[1]]
@@ -105,23 +106,29 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
     #############################################
     #   STARTS ITERATION OVER SPECTRA BATCH     #
     #############################################
-
+        
+        FITFAIL = 0
         logging.info("Starting iteration over spectra...\n")
         for ITERATION in range(dimension):
                
             spec = currentspectra
             RAW = SpecRead.getdata(spec)
             
-            #############################
-            #  FIT OR NOT THE SPECFILE  #
-            #############################
+            #######################################
+            #     ATTEMPT TO FIT THE SPECFILE     #
+            # PYMCAFIT DOES NOT USE 2ND DIF CHECK #
+            #######################################
 
             if peakmethod == 'PyMcaFit': 
                 try:
+                    usedif2 = False
                     specdata = SpecFitter.fit(spec)
                 except:
+                    FITFAIL += 1
+                    usedif2 = True
                     print("\tCHANNEL COUNT METHOD USED FOR {0}!\t".format(spec))
-                    logging.warning("\tFIT FAILED! USING CHANNEL COUNT METHOD!\t")
+                    logging.warning("\tFIT FAILED! USING CHANNEL COUNT METHOD FOR {0}!\t"\
+                            .format(spec))
                     specdata = SpecRead.getdata(spec)
             elif peakmethod == 'simple_roi': specdata = SpecRead.getdata(spec)
             elif peakmethod == 'auto_roi': specdata = SpecRead.getdata(spec)
@@ -132,7 +139,7 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
             #  SETS THE BACKGROUND  #
             #########################
             
-            if svg == 'SNIPBG': 
+            if bgstrip == 'SNIPBG': 
                 background = SpecMath.peakstrip(RAW,24,3)
                 logging.debug('SNIPGB calculated for spec {0}'.format(spec))
             else: background = np.zeros([len(specdata)])
@@ -171,7 +178,8 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
             logging.info("Specfile being processed is: {0}\n".format(spec))
  
             if peakmethod == 'auto_roi' or peakmethod == 'PyMcaFit':
-                ka_info = SpecMath.getpeakarea(kaenergy,specdata,energyaxis,background,svg,RAW)
+                ka_info = SpecMath.getpeakarea(\
+                        kaenergy,specdata,energyaxis,background,configdict,RAW,usedif2)
                 ka = ka_info[0]
             
                 for channel in range(len(specdata)):
@@ -183,7 +191,7 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
                 if ka == 0: kb = 0
                 elif ka > 0:
                     kb_info = SpecMath.getpeakarea(\
-                            kbenergy,specdata,energyaxis,background,svg,RAW)
+                            kbenergy,specdata,energyaxis,background,configdict,RAW,usedif2)
                     kb = kb_info[0]
 
                     for channel in range(len(specdata)):
@@ -231,6 +239,11 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
 
             #####################################
 
+#            plt.plot(energyaxis,specdata,label='specdata')
+#            plt.plot(energyaxis,RAW,label='raw')
+#            plt.legend()
+#            plt.show()
+            
             #########################################
             #   UPDATE ELMAP POSITION AND SPECTRA   #
             #########################################        
@@ -255,6 +268,7 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
             logging.warning("ValueError, elmap.max() = {0}!".format(elmap.max()))
         timestamp = time.time() - partialtimer
         print("Execution took %s seconds" % (timestamp))
+        if peakmethod == 'PyMcaFit': print("Fit fail: {0}%".format(100*FITFAIL/dimension))
         
         timestamps = open(SpecRead.workpath + '/timestamps.txt'\
                     .format(Element,SpecRead.DIRECTORY),'a')
@@ -265,6 +279,7 @@ def getpeakmap(Element,ratio=configdict.get('ratio'),plot=None,\
 
         plt.plot(energyaxis,CUMSUM,label='CUMSUM CURRENT DATA')
         plt.plot(energyaxis,CUMSUM_RAW,label='CUMSUM RAW DATA')
+        plt.legend()
         plt.show()
                         
         ##################################

SpecMath.py
@@ -111,11 +111,11 @@ def setROI(lookup,xarray,yarray,svg):
         idx = 0
         while xarray[idx] <= lowx:
             idx+=1
-        lowx_idx = idx-2
+        lowx_idx = idx-3
         logging.debug("lowx_idx: %d" % lowx_idx)
         while xarray[idx] <= highx:
             idx+=1
-        highx_idx = idx+2
+        highx_idx = idx+3
         logging.debug("highx_idx: %d" % highx_idx)
         ROIaxis = xarray[lowx_idx:highx_idx]
         ROIdata = yarray[lowx_idx:highx_idx]
@@ -136,16 +136,16 @@ def setROI(lookup,xarray,yarray,svg):
         
         logging.debug("ROI[0] = {0}, ROI[-1] = {1}".format(ROIaxis[0],ROIaxis[-1]))
     
-    lowx_idx = lowx_idx + 2
-    highx_idx = highx_idx - 2
-    peak_center = shift[2]-2
+    lowx_idx = lowx_idx + 3
+    highx_idx = highx_idx - 3
+    peak_center = shift[2]-3
     logging.debug("peak_center = {0}".format(peak_center)) 
     return lowx_idx,highx_idx,peak_center,isapeak
 
-def getpeakarea(lookup,data,energyaxis,continuum,svg,RAW):
+def getpeakarea(lookup,data,energyaxis,continuum,localconfig,RAW,usedif2):
     Area = 0
-    
-    idx = setROI(lookup,energyaxis,data,svg)
+   
+    idx = setROI(lookup,energyaxis,data,localconfig.get('bgstrip'))
     isapeak = idx[3]
     xdata = energyaxis[idx[0]:idx[1]]
     ydata = data[idx[0]:idx[1]]
@@ -157,42 +157,56 @@ def getpeakarea(lookup,data,energyaxis,continuum,svg,RAW):
     if isapeak == True: 
         if original_data.sum() <= (3*ROIbg.sum()): isapeak = False
     
-    try: 
-        smooth_dif2 = scipy.signal.savgol_filter(getdif2(RAW,energyaxis,1),5,3)
-        smooth_dif2 = smooth_dif2[idx[0]:idx[1]]
-    except:
-        loggin.warning("Cannot apply savgol filter to 2nd derivate! Vector is too short!")
-        smooth_dif2 = getdif2(RAW,energyaxis,1)
-        raise Exception("Cannot smooth 2nd derivate!")
-        pass
+    ##########################
+    # 2ND DIFFERENTIAL CHECK #
+    ##########################
+    if usedif2 == True and isapeak == True:
+        try: 
+            smooth_dif2 = scipy.signal.savgol_filter(getdif2(RAW,energyaxis,1),5,3)
+            smooth_dif2 = smooth_dif2[idx[0]:idx[1]]
+        except:
+            logging.warning("Cannot apply savgol filter to 2nd derivate! Vector is too short!")
+            smooth_dif2 = getdif2(RAW,energyaxis,1)
+            raise Exception("Cannot smooth 2nd derivate!")
+            pass
     
-    for i in range(len(smooth_dif2)):
-        if smooth_dif2[i] < -1: smooth_dif2[i] = smooth_dif2[i]
-        elif smooth_dif2[i] > -1: smooth_dif2[i] = 0
+        ##################################
+        # 2ND DIFFERENTIAL CUTOFF FILTER #
+        ##################################
     
-# 2ND DERIVATIVE CHECK - LEFT AND RIGHT - #    
-
-    if smooth_dif2[idx[2]] < 0 and isapeak == True\
-            or smooth_dif2[idx[2]+1] < 0 and isapeak == True\
-            or smooth_dif2[idx[2]-1] < 0 and isapeak == True:
-        logging.debug("Dif2 is: {0}".format(smooth_dif2[idx[2]]))
-        logging.debug("Dif2 left = {0} and Dif2 right = {1}".format(\
+        for i in range(len(smooth_dif2)):
+            if smooth_dif2[i] < -1: smooth_dif2[i] = smooth_dif2[i]
+            elif smooth_dif2[i] > -1: smooth_dif2[i] = 0
+    
+        if smooth_dif2[idx[2]] < 0 or smooth_dif2[idx[2]+1] < 0 or smooth_dif2[idx[2]-1] < 0:
+            logging.debug("Dif2 is: {0}".format(smooth_dif2[idx[2]]))
+            logging.debug("Dif2 left = {0} and Dif2 right = {1}".format(\
+                    smooth_dif2[idx[2]-1],smooth_dif2[idx[2]+1]))
+            for i in range(len(xdata)):
+                if ROIbg[i] < ydata[i]: Area += (ydata[i]-ROIbg[i])
+                logging.debug("Area: {0}\t Estimated BG: {1}".format(ydata[i],ROIbg[i]))
+        else: 
+            logging.debug("{0} has no peak! Dif2 = {1} and isapeak = {2}\n"\
+                    .format(lookup,smooth_dif2[idx[2]],isapeak))
+            logging.debug("Dif2 left = {0} and Dif 2 right = {1}".format(
                 smooth_dif2[idx[2]-1],smooth_dif2[idx[2]+1]))
+    
+    ##########################
+
+    elif localconfig.get('peakmethod') == 'PyMcaFit' and usedif2 == False:
+        logging.info("No dif 2 criteria for {0} method".format(localconfig.get('peakmethod')))
+        smooth_dif2 = np.zeros([len(xdata)])
         for i in range(len(xdata)):
             if ROIbg[i] < ydata[i]: Area += (ydata[i]-ROIbg[i])
             logging.debug("Area: {0}\t Estimated BG: {1}".format(ydata[i],ROIbg[i]))
-                
-    else: 
-        logging.debug("{0} has no peak! Dif2 = {1}\n".format(lookup,smooth_dif2[idx[2]]))
-        logging.debug("Dif2 left = {0} and Dif 2 right = {1}".format(
-            smooth_dif2[idx[2]-1],smooth_dif2[idx[2]+1]))
-
+               
     ##############
     # TEST BLOCK #          
     TESTFNC = False
     ##############    
     
-    if TESTFNC == True:    
+    if TESTFNC == True:
+ 
         print(5*"*" + " IS A PEAK? " + 5*"*" + "\n\t{0}\t".format(isapeak))
         print("RAW peak: {0}\t bg: {1}\nyDATA peak: {2}"\
                .format(original_data.sum(),(3*ROIbg.sum()),ydata.sum()))
@@ -205,15 +219,17 @@ def getpeakarea(lookup,data,energyaxis,continuum,svg,RAW):
 
         print("ydata = {0}".format(ydata))
         print("len(ydata) = {0}".format(len(ydata)))
-        print("ydata[idx[2]] = {0}, smooth_dif2[idx[2]] = {1}".format(\
+        try: print("ydata[idx[2]] = {0}, smooth_dif2[idx[2]] = {1}".format(\
                 ydata[idx[2]],smooth_dif2[idx[2]]))
+        except: pass
         print("idx[2] = {0}".format(idx[2]))
         print("Lookup: {0} Area: {1}\n".format(lookup,Area))
     
-        plt.semilogy(xdata,ydata, label='data')
-        plt.semilogy(xdata,original_data, label='RAW data')
-        plt.semilogy(xdata,smooth_dif2, label='2nd Differential')
-        plt.semilogy(xdata,ROIbg, label='BG estimation')
+        plt.plot(xdata,ydata, label='data')
+        plt.plot(xdata,original_data, label='RAW data')
+        try: plt.plot(xdata,smooth_dif2, label='2nd Differential')
+        except: pass
+        plt.plot(xdata,ROIbg, label='BG estimation')
         plt.legend()
         plt.show()
     