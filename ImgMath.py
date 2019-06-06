775fdb76809b9a323ed1f9f103e27c8c3a1b9f7b
linssab <sergiolins_@outlook.com>
2019-06-06 23:19:57 +0200

    added colorbar to final plot window

--- ImgMath.py
+++ ImgMath.py
@@ -22,6 +22,12 @@ import cv2
 configdict = SpecRead.CONFIG
 
 def colorbar(mappable):
+    
+    ##############################################
+    # FROM:                                      #
+    # https://joseph-long.com/writing/colorbars/ #
+    ##############################################
+    
     ax = mappable.axes
     fig = ax.figure
     divider = make_axes_locatable(ax)
@@ -67,12 +73,13 @@ def getheightmap(depth_matrix,mask,thickratio,compound):
                 heightfile.write("%d\t%d\n" % (i, j))
     return heightmap
 
-#####################################################
-#   set_axes_equal(ax) FUNCTION OBTAINED FROM:      #
-#   https://stackoverflow.com/questions/13685386    #
-#####################################################
-
 def set_axes_equal(ax):
+    
+    #####################################################
+    #   set_axes_equal(ax) FUNCTION OBTAINED FROM:      #
+    #   https://stackoverflow.com/questions/13685386    #
+    #####################################################
+
     '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
     cubes as cubes, etc..  This is one possible solution to Matplotlib's
     ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
@@ -255,11 +262,13 @@ def split_and_save(map_array,element_list,configdict):
         if len(element_list) > 1: ax = axs[Element]
         else: ax=axs
         fig_list.append(ax.imshow(image,cmap='gray'))
+        colorbar(fig_list[Element])
         if configdict.get('ratio') == False:
             ax.set_title(element_list[Element]+' alpha line')
         else: ax.set_title(element_list[Element])
         if imagex > target_size or imagey > target_size: large_image = image
         else: large_image = cv2.resize(image,(newY,newX),interpolation=cv2.INTER_NEAREST)
+        
         cv2.imwrite(SpecRead.workpath+'/output/'+SpecRead.DIRECTORY+
             '/{0}_bgtrip={1}_ratio={2}_enhance={3}_peakmethod={4}.png'\
             .format(element_list[Element],configdict.get('bgstrip'),configdict.get('ratio')\
@@ -267,9 +276,6 @@ def split_and_save(map_array,element_list,configdict):
 
     ##################################################
     
-    #plt.colorbar(fig_list[0],ax=axs, orientation = 'horizontal',fraction=.1)
-    #map_ = plt.imshow(axs)
-    #plt.colorbar(map_, orientation = 'vertical')
     fig.savefig(SpecRead.workpath+'/output/'+SpecRead.DIRECTORY+
             '/elements_plot_bgtrip={0}_ratio={1}_enhance={2}_peakmethod={3}.png'\
             .format(configdict.get('bgstrip'),configdict.get('ratio')\
