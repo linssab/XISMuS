ba04d04ebb036c3f49bdb248fd9de8e8b37e4d34
linssab <sergio.lins@roma3.infn.it>
2019-10-28 18:06:04 +0100

    Plotting vertical lines instead of gaussian and considering only lines with a rate superior to 19.99%

--- CheckPeaks.py
+++ CheckPeaks.py
@@ -5,7 +5,7 @@
 #                                                               #
 #################################################################
 
-import sys
+import sys, os
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -18,6 +18,7 @@ ElementList = [ elt[0] for elt in Elements.ElementsInfo ]
 ElementBinding=Elements.ElementBinding
 ElementsInfo=Elements.ElementsInfo
 
+SpecRead.setup()
 """
 DEFINE THE GLOBAL VAR FOR CALCULATING G(i,Ej) AND s
 """
@@ -61,7 +62,7 @@ for i in range(n):
 if __name__=="__main__":
     ele = sys.argv[1]
     if ele not in ElementList: raise ValueError("{0} not an element!".format(ele))
-    Analytic=[]
+    Analytic, Rate = [],[]
     for rays in Elements.Element[ele]['rays']:
         print(rays, ":")
         for transition in Elements.Element[ele][rays]:
@@ -69,19 +70,28 @@ if __name__=="__main__":
                     (transition, Elements.Element[ele][transition]['energy'],\
                     Elements.Element[ele][transition]['rate']))
             Analytic.append(Elements.Element[ele][transition]['energy']*1000)
-    print("Every transition for element %s : %r" % (ele,Analytic))
+            Rate.append(Elements.Element[ele][transition]['rate'])
+    #print("Every transition for element %s : %r" % (ele,Analytic))
     
     cube_file = open(SpecRead.cube_path,'rb')
     datacube = pickle.load(cube_file)
     cube_file.close()
+
+    #specname = os.getcwd()+"\\tin_600s.mca"
+    #single_data = SpecRead.getdata(specname)
+    #single_data = single_data/single_data.max()
 
     energies = Analytic
     lines = GiEj(energies)
     lines = lines/lines.max()
     stacksum = datacube.sum
     stacksum = stacksum/stacksum.max()
-    plt.plot(SpecMath.energyaxis(),lines,label="{0} Lines".format(ele))
-    plt.plot(SpecMath.energyaxis(),stacksum,label="Stacksum")
+    #plt.plot(SpecMath.energyaxis(),lines,label="{0} Lines".format(ele))
+    plt.plot(SpecMath.energyaxis(),stacksum,label="Stacksum",color="red")
+    for i in range(len(energies)):
+        if energies[i]/1000 < 35 and Rate[i] > 0.20:
+            plt.axvline(x=energies[i]/1000,color="blue", linestyle="--")
+    #plt.plot(SpecMath.energyaxis(),single_data,label=specname)
     plt.legend()
     plt.show()
 
