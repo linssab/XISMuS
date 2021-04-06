6ed0bb07c4b11e9831bc770119c0ce2469ee8498
linssab <sergio.lins@roma3.infn.it>
2021-04-06 16:39:46 +0200

    Added update code

---
+++ CustomScripts/update.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	printf("Starting patcher...");
+	char *patch_exe;
+	char *xismus_exe;
+	patch_exe = argv[1];
+	printf("\n");
+	//for (int i=1; i<argc; i++){
+	//	printf("\nPATH:\n%s",argv[i]);
+	//}
+	printf("\n%s",patch_exe);
+	if (system(patch_exe) == 0){
+		printf("\nDone patching");
+		if (remove(patch_exe) != 0){
+			printf("\nCould not delete patch.exe");
+		} else {
+			printf("\nRemoved patch.exe");
+			exit(1);
+		}
+	}
+	exit(0);
+}
