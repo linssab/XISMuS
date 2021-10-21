712ecb5ef3ed7576611f7064461f5fcc506ac77d
linssab <sergio.lins@roma3.infn.it>
2021-10-21 14:50:57 +0200

    Replaced Elements with the compwizard package

--- Elements/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-logger = logging.getLogger("logfile")
-logger.info("Booting Elements...")
-
-logger.info("Importing EnergyLib...")
-from .EnergyLib import *
-logger.info("Importing Compounds...")
-from .Compounds import *
-logger.info("Elements ready!")
