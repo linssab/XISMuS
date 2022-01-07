af27d1e234974263c8575157e9fc1590a42c9ed6
linssab <sergio.lins@roma3.infn.it>
2022-01-07 15:37:23 +0100

    Added works on CubeViewer gui

--- GUI/CubeViewer.py
+++ GUI/CubeViewer.py
@@ -30,6 +30,7 @@ import numpy as np
 import copy
 logger = logging.getLogger("logfile")
 from .Utils import *
+from .ProgressBar import *
 #############
 
 ######################
@@ -80,7 +81,7 @@ class ImageWindow:
         __self__.canvas = FigureCanvasTkAgg(__self__.figure,__self__.upper)
         __self__.canvas.draw()
         __self__.mplCanvas = __self__.canvas.get_tk_widget()
-        __self__.mplCanvas.pack(fill=BOTH, anchor=N+W,expand=True)
+        __self__.mplCanvas.pack(fill=BOTH, anchor=N+W, expand=True)
         __self__.canvas._tkcanvas.pack()
         __self__.master.protocol("WM_DELETE_WINDOW",__self__.wipe_plot)
         __self__.master.after(100,__self__.master.attributes,"-alpha",1.0)
@@ -105,18 +106,111 @@ class ImageWindow:
         __self__.display.set_cmap(Constants.COLORMAP)
         __self__.canvas.draw()
 
-    def create_connection(__self__):
+    def create_connection(__self__, datacube):
+        __self__.DATACUBE = datacube
+        __self__.start = IntVar()
+        __self__.pixels = IntVar()
+        __self__.start.set(0)
+        __self__.pixels.set(0)
+
+        __self__.LeftPanel = Frame(__self__.master, 
+                height=__self__.master.winfo_height())
+        __self__.LeftPanel.pack(side=RIGHT)
+        __self__.LabelStart = Label(__self__.LeftPanel, text="Line start")
+        __self__.LabelPixels = Label(__self__.LeftPanel, text="Pixels to move")
+        create_tooltip(__self__.LabelPixels, "Positive values moves to the right, negative values move to the left.")
+        __self__.EntryStart = ttk.Entry(
+                __self__.LeftPanel, 
+                textvariable=__self__.start, 
+                width=5)
+        __self__.EntryPixels = ttk.Entry(
+                __self__.LeftPanel, 
+                textvariable=__self__.pixels,
+                width=5)
+        __self__.BtnEntryOk = ttk.Button(__self__.LeftPanel, text="Move lines", 
+                command=__self__.move_callback)
+        __self__.BtnOk = ttk.Button(__self__.LeftPanel, text="Fix!", width=13, 
+                command=__self__.execute)
+        __self__.BtnCancel = ttk.Button(__self__.LeftPanel, text="Cancel", width=13,
+                command=__self__.wipe_plot)
+
+        padx, pady= 12, 12
+        __self__.LabelStart.grid(row=0, column=0, padx=padx)
+        __self__.LabelPixels.grid(row=1, column=0, padx=padx)
+        __self__.EntryStart.grid(row=0, column=1, padx=padx)
+        __self__.EntryPixels.grid(row=1, column=1, padx=padx)
+        __self__.BtnEntryOk.grid(row=2, column=0, padx=padx, pady=(pady,0))
+        __self__.BtnOk.grid(row=3, column=0, pady=(pady,0))
+        __self__.BtnCancel.grid(row=3, column=1, pady=(pady,0))
+
         __self__.toolbar = NavigationToolbar(__self__.canvas,__self__.lower)
         __self__.toolbar.update()
         __self__.master.update_idletasks()
         __self__.Clicker = Clicker(__self__)
         __self__.Clicker.connect()
 
+    def execute(__self__, e=""):
+        path = __self__.Writer.file_path()
+        try: f = open(path, "r")
+        except FileNotFoundError:
+            messagebox.showerror("Error!","Data could not be written! Try re-running the software wth administrator rights. If error persists, please contact us.")
+            return
+        cutL = 0
+        cutR = -1
+        lines = f.readlines()
+        for line in lines[1:]:
+            row, px = line.split("\t")
+            row = int(row)
+            px = int(px)
+            __self__.DATACUBE.matrix[row,:,:] = np.roll(
+                    __self__.DATACUBE.matrix[row,:,:], px, 0)
+            if px>0:
+                if px > cutL: cutL = px
+            elif px<0:
+                if px < cutR: cutR = px
+        __self__.DATACUBE.matrix = __self__.DATACUBE.matrix[ :, cutL:cutR, : ]
+        __self__.parent.check_open_analyzers(__self__.DATACUBE.name)
+        __self__.parent.wipe()
+        busy = BusyManager(__self__.master)
+        busy.busy()
+        bar = Busy(6,0)
+        bar.update_text("Working...")
+        __self__.DATACUBE.wipe_maps()
+        __self__.DATACUBE.dimension = __self__.DATACUBE.matrix.shape[0], __self__.DATACUBE.matrix.shape[1]
+        __self__.DATACUBE.specsize = __self__.DATACUBE.matrix.shape[-1]
+        __self__.DATACUBE.img_size = __self__.DATACUBE.dimension[0] * __self__.DATACUBE.dimension[1]
+        mps = np.zeros([__self__.DATACUBE.matrix.shape[2]],dtype="int32")
+        bar.updatebar(1)
+        bar.update_text("Rechecking MPS...")
+        __self__.DATACUBE.MPS(mps)
+        bar.updatebar(2)
+        bar.update_text("Checking sum-spec...")
+        __self__.DATACUBE.stacksum()
+        bar.updatebar(3)
+        bar.update_text("Recreating densemap...")
+        __self__.DATACUBE.create_densemap()
+        bar.updatebar(4)
+        bar.update_text("Refitting Fano and Noise...")
+        __self__.DATACUBE.fit_fano_and_noise()
+        bar.updatebar(5)
+        bar.update_text("Saving CUBE to disk...")
+        __self__.DATACUBE.save_cube()
+        bar.updatebar(6)
+        bar.update_text("Done!")
+        busy.notbusy()
+        __self__.wipe_plot(e="loadcube")
+        bar.destroybar()
+
+    def move_callback(__self__, e=""):
+        __self__.Clicker.move_batch()
+
     def remove_connection(__self__):
         __self__.Clicker.disconnect()
 
-    def wipe_plot(__self__):
+    def wipe_plot(__self__, e=""):
         __self__.parent.master.focus_set()
+        if e == "loadcube":
+            __self__.parent.sample_select(force=__self__.DATACUBE.name)
         __self__.master.destroy()
         del __self__
 
@@ -129,7 +223,6 @@ class Clicker:
     def getclick(__self__,e=""):
         if e.xdata is not None and e.ydata is not None:
             __self__.x, __self__.y = round(e.xdata), round(e.ydata)
-            print("X, Y = ",__self__.x,__self__.y)
         else: return
         __self__.pop()
 
@@ -206,6 +299,14 @@ class Clicker:
             __self__.win.destroy()
             del __self__.win
 
+    def move_batch(__self__):
+        px = __self__.parent.pixels.get()
+        start = __self__.parent.start.get()
+        for i in range(start, __self__.parent.image.shape[0], 2):
+            __self__.parent.image[i,:] = np.roll(__self__.parent.image[i,:], px, 0)
+            __self__.parent.Writer.put(i, px)
+        __self__.parent.refresh()
+
     def dummy(__self__,e=""):
         value = __self__.en.get()
         row = int(__self__.y)
@@ -230,4 +331,7 @@ class Writer:
         f = open(__self__.path,"a")
         f.write("\n{0}\t{1}".format(row,value))
         f.close()
+
+    def file_path(__self__):
+        return __self__.path
 
